f17486e7c69ca506fb7c2368d06369a6774872d0
Guilherme Lopes <guilhermelopes17@hotmail.com>
2022-10-08 23:17:23 -0300

    iniciando projeto

---
+++ src/main/java/com/guilherme/getvoucher/GetvoucherApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.getvoucher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetvoucherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetvoucherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilherme/getvoucher/GetvoucherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.getvoucher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetvoucherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
